14a78e828c63e1df617d3afca5daa4a9dfef6fc8
AscendedWolf50 <builesjuanjo10@gmail.com>
2025-03-04 13:35:39 -0500

    TallerPoo
    Docs

---
+++ TallerPOO/BT/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerPOO.BT
+{
+    public abstract class Node
+    {
+        public abstract bool Execute(); // Método base para todos los nodos
+    }
+
+}
+
+
